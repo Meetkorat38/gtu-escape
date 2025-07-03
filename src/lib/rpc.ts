958ac6711f52dc840c62a366b8d23aa1b6fa4a43
Meetkorat38 <meetkorat903@gmail.com>
2025-07-03 12:13:19 +0530

    fix: update environment variables and refactor base URL logic in RPC client

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,11 +1,8 @@
 import { hc } from "hono/client";
 import { AppType } from "@/app/api/[[...route]]/route";
 
-const baseUrl =
-  typeof window === "undefined"
-    ? process.env.NODE_ENV === "production"
+const baseUrl = process.env.NODE_ENV === "production"
       ? process.env.NEXT_PUBLIC_API_URL!
       : "http://localhost:3000"
-    : "/api";
 
 export const client =  hc<AppType>(baseUrl);
